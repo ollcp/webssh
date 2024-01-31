32eb1900564130a914554db3a0e535f619e1c8e8
ollcp <cuishuaihua@163.com>
2024-01-31 15:42:13 +0800

    update

--- localbuild.sh
+++ localbuild.sh
@@ -3,7 +3,10 @@
 cd web && npm install && npm run build
 
 cd ..
-GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o webssh
+GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o webssh_linux_amd64
 
+exit
+
+mkdir -p docker/result
 cp webssh docker/result/webssh_linux_amd64
-cd docker && docker build -t webssh .
+cd docker && docker buildx build --platform linux/amd64 -t work.cribc.com:5000/ibps/webssh:v1.26 . --push
---
+++ webssh.spec
@@ -0,0 +1,17 @@
+Name:     webssh
+Version:  1.26
+Release:  1
+Summary:  webssh
+License:  GPLv3+
+
+%description
+webssh
+
+%install
+mkdir -p %{buildroot}/usr/local/bin
+install -m 755 -t %{buildroot}/usr/local/bin webssh_linux_amd64
+
+
+%files
+/usr/local/bin/webssh_linux_amd64
+
